c4f4f2e04b18bf5e6c200e8cc8de0cab49d04f16
anton <anton@adminpc.Dlink>
2023-08-30 21:04:21 +0300

    fixes added

--- main.py
+++ main.py
@@ -1,56 +1,45 @@
-import json
 import requests
 from dotenv.main import load_dotenv
 from urllib.parse import urlparse
 import os
 
 
-def send_request(url, method, headers=None, body=None):
-    if method == 'GET':
-        response = requests.get(url, headers=headers)
-    elif method == 'POST':
-        response = requests.post(url, headers=headers, data=json.dumps(body))
-    else:
-        raise ValueError("Неподдерживаемый метод запроса. Поддерживаются только 'GET' и 'POST'.")
-    response.raise_for_status()
-    return response
-
-
 def shorten_link(long_url, headers):
-    send_request(long_url, method='GET')
     url = 'https://api-ssl.bitly.com/v4/bitlinks'
     body = {"long_url": long_url}
-    response = send_request(url, 'POST', headers=headers, body=body)
+    response = requests.post(url, headers=headers, json=body)
+    response.raise_for_status()
     return response.json()['id']
 
 
 def count_clicks(bitlink, headers):
     url = f'https://api-ssl.bitly.com/v4/bitlinks/{strip_scheme(bitlink)}/clicks/summary'
-    response = send_request(url, method='GET', headers=headers)
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
     return response.json()['total_clicks']
 
 
 def is_bitlink(bitlink, headers):
-    try:
-        schemaless_url = strip_scheme(bitlink)
-        url = 'https://api-ssl.bitly.com/v4/bitlinks/{}'
-        send_request(url.format(schemaless_url), method='GET', headers=headers)
+    schemaless_url = strip_scheme(bitlink)
+    url = 'https://api-ssl.bitly.com/v4/bitlinks/{}'
+    response = requests.get(url.format(schemaless_url), headers=headers)
+    if response.ok:
         return True
-    except requests.HTTPError:
-        return False
+    return False
 
 
 def strip_scheme(url):
-    schemaless = urlparse(url)._replace(scheme='').geturl()
-    return schemaless[2:] if schemaless.startswith("//") else schemaless
+    parser_url = urlparse(url)
+    schemaless = parser_url.netloc + parser_url.path
+    return schemaless
 
 
 if __name__ == '__main__':
     load_dotenv()
-    TOKEN = os.environ['TOKEN']
+    token = os.environ['BITLY_TOKEN']
 
     bitlink_headers = {
-        'Authorization': f'Bearer {TOKEN}',
+        'Authorization': f'Bearer {token}',
         'Content-Type': 'application/json'
     }
 
